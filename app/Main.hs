fd402fb68c96d8cafca18e4240961d3c0327508f
Jun <me@noraesae.net>
2016-10-05 02:56:00 +0900

    Implement Main module

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
-import GitHub (didYouCommitToday)
+import GitHub
+import LineNotify
+import Messages
 
 main :: IO ()
-main = didYouCommitToday >>= print
+main = do
+  yesIDid <- didYouCommitToday
+  message <- if yesIDid then goodJob else saiteiDesu
+  send message
